88c869294b51fcebf689b3ee9d23d9f09cc1022c
comet33z <86732559@qq.com>
2019-12-20 14:31:56 +0800

    ~

---
+++ Assets/Scripts/Monsters/FlyShit.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyShit : BirdBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentStatus == BirdStatus.BirdStatus_Normal)
+        {
+            Vector3 move = Vector3.left * SpeedX * Time.deltaTime;
+            this.transform.Translate(move);
+        }
+        else if (CurrentStatus == BirdStatus.BirdStatus_ToHotPot)
+        {
+
+            float GritySpeed = -10f * (FlyHotPotTime);
+
+            //SpeedY -= 10  * Time.deltaTime;
+            Vector3 move = (SpeedY + GritySpeed) * Time.deltaTime * Vector3.up;
+            this.transform.position += move;
+
+            FlyHotPotTime += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Sword"))
+        {
+            if (this.CurrentStatus == BirdStatus.BirdStatus_Normal)
+            {
+                FlyToHotPot();
+            }
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("SwordInvalid"))
+        {
+            Debug.Log("Invalid !!!!");
+            if (this.CurrentStatus == BirdStatus.BirdStatus_Normal)
+            {
+                FlyToHotPot();
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/Players/DarkCloud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkCloud : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Sword"))
+        {
+            Debug.Log("GameOver");
+            SwordAutoController sac = GameObject.FindObjectOfType<SwordAutoController>();
+            if(sac)
+            {
+                sac.CurrentStatus = SwordStatus.SwordStatus_GameOver;
+            }
+        }
+    }
+}
